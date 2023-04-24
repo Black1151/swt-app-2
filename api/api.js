c25f054b029cb6522bfe66ffcf2f24b31d6ff7b7
blackPrawn <danblack9988@gmail.com>
2023-04-24 14:33:57 +0100

    add API calls for goal CRUD operrations and factor our common logic

--- api/api.js
+++ api/api.js
@@ -3,26 +3,66 @@ import axios from "axios";
 const server = "192.168.0.56";
 const port = "3333";
 
-export const getAllStudentsAPI = async () => {
+const apiCall = async (method, url, data) => {
   try {
-    const response = await axios.get(`http://${server}:${port}/api/students`);
-    console.log("response", response.data);
-    return response.data;
+    const response = await axios({ method, url, data });
+    return response;
   } catch (error) {
     console.error(error);
     return null;
   }
 };
 
+export const getAllStudentsAPI = async () => {
+  const response = await apiCall(
+    "get",
+    `http://${server}:${port}/api/students`
+  );
+  console.log("response", response.data);
+  return response.data;
+};
+
 export const addStudentAPI = async (student) => {
-  try {
-    const response = await axios.post(
-      `http://${server}:${port}/api/students`,
-      student
-    );
-    return response.status;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  const response = await apiCall(
+    "post",
+    `http://${server}:${port}/api/students`,
+    student
+  );
+  return response.status;
+};
+
+export const updateBehaviorScoreAPI = async (studentId, behaviorScore) => {
+  const response = await apiCall(
+    "put",
+    `http://${server}:${port}/api/students/${studentId}/behavior_score`,
+    { behavior_score: behaviorScore }
+  );
+  return response.status;
+};
+
+export const addGoalAPI = async (studentId, goal) => {
+  const response = await apiCall(
+    "post",
+    `http://${server}:${port}/api/students/${studentId}/goals`,
+    goal
+  );
+  return response.status;
+};
+
+export const updateGoalStatusAPI = async (studentId, goalIndex, status) => {
+  const response = await apiCall(
+    "put",
+    `http://${server}:${port}/api/students/${studentId}/goals/${goalIndex}/status`,
+    { status }
+  );
+  return response.status;
+};
+
+export const addEvidenceAPI = async (studentId, goalIndex, evidence) => {
+  const response = await apiCall(
+    "post",
+    `http://${server}:${port}/api/students/${studentId}/goals/${goalIndex}/evidence`,
+    evidence
+  );
+  return response.status;
 };

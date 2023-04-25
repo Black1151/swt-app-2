7e333d812d93ca6545163a94baa6cd7a3d694146
blackPrawn <danblack9988@gmail.com>
2023-04-25 10:27:01 +0100

    update student profile page to render list of goals from the backend

--- src/screens/StudentProfile.jsx
+++ src/screens/StudentProfile.jsx
@@ -1,42 +1,68 @@
-import React from 'react';
-import { ScrollView, VStack, HStack, Text, Heading, Badge, Button } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, VStack, HStack, Text, Heading, Badge, Button, Input } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/core';
 import theme from '../../theme/theme';
+import { addGoalAPI, getAllGoalsAPI } from '../../api/api';
+import { TouchableOpacity } from 'react-native'
+
 
 const StudentProfile = () => {
   const navigation = useNavigation();
-  const student = {
-    name: 'John Doe',
-    year: 'Senior',
-    goals: [
-      { id: 1, title: 'Improve my grades', status: 'In Progress' },
-      { id: 2, title: 'Get more involved in clubs', status: 'Not Started' },
-      { id: 3, title: 'Build a portfolio', status: 'Completed' },
-    ],
+  const { student } = useRoute().params;
+  const [newGoal, setNewGoal] = useState('');
+  const [goalList, setGoalList] = useState([]);
+
+  const handleAddGoal = async () => {
+    addGoalAPI(student.id, newGoal)
+    setNewGoal('');
+    const updatedGoals = await getAllGoalsAPI(student.id);
+    setGoalList(updatedGoals);
   };
+
+  useEffect(() => {
+    const getGoals = async () => {
+      let _goals = await getAllGoalsAPI(student.id);
+      setGoalList(_goals); 
+    };
+    getGoals(); 
+  }, []);
 
   return (
     <ScrollView>
       <VStack p={4} space={4}>
-        <Heading size="xl">{student.name}</Heading>
-        <Text fontSize="md">{student.year}</Text>
+        <Heading size="xl">{student.first_name} {student.last_name}</Heading>
+        <Text fontSize="md">Year: {student.year}</Text>
+        <Text
+          fontSize="md"
+        >Student behaviour: {student.behavior_score}</Text>
         <Heading size="md">Goals:</Heading>
         <VStack space={2} divider={<Text fontSize="sm">|</Text>}>
-          {student.goals.map((goal, index) => (
+        {goalList[0] ? goalList.map((goal, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate('StudentGoal', { goal })}
+          >
             <HStack
-              key={index}
               alignItems="center"
               justifyContent="space-between"
-              onPress={() => navigation.navigate('GoalDetails', { goal })}
             >
               <Text fontSize="md">{goal.title}</Text>
               <Badge variant="solid" colorScheme={theme.colors.primary[500]}>
                 {goal.status}
               </Badge>
             </HStack>
-          ))}
+          </TouchableOpacity>
+        )) : <Text>No goals set, please add below</Text>
+        }
         </VStack>
-        <Button colorScheme="primary" onPress={() => navigation.navigate('AddGoal')}>
+        <Input
+          placeholder="Goal to add"
+          value={newGoal}
+          onChangeText={setNewGoal}
+          mb={4}
+        />
+        <Button colorScheme="primary" onPress={handleAddGoal}>
           Add Goal
         </Button>
       </VStack>

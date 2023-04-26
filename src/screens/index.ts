c9d8095edf54d36f6543609a942bde2a0bab9e1d
blackPrawn <danblack9988@gmail.com>
2023-04-26 10:20:18 +0100

    update page index

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export { default as AddStudent } from "./AddStudent";
 export { default as StudentProfile } from "./StudentProfile";
 export { default as Dashboard } from "./Dashboard";
 export { default as GoalDetails } from "./GoalDetails";
+export { default as AddProgress } from "./AddProgress";
